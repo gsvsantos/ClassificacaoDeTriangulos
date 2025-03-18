27c155693ebd042288cf07e10bdb506c3b30a7c2
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-18 17:02:50 -0300

    refactor: Criado métodos para melhor desempenho e organização do programa. - Reestruturação completa do código.

--- ClassificacaoDeTriangulos.ConsoleApp/Program.cs
+++ ClassificacaoDeTriangulos.ConsoleApp/Program.cs
@@ -6,49 +6,25 @@
         {
             do
             {
+                Header();
+                int sideX = InputReader("X");
+                int sideY = InputReader("Y");
+                int sideZ = InputReader("Z");
 
                 Header();
-                Console.Write("Informe o valor do X: ");
-                int sideX = Convert.ToInt32(Console.ReadLine());
+                ShowValues(sideX, sideY, sideZ);
 
-                Console.Write("Informe o valor do Y: ");
-                int sideY = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Informe o valor do Z: ");
-                int sideZ = Convert.ToInt32(Console.ReadLine());
-
-                Header();
-                Console.WriteLine("Valores Informados:");
-                Console.WriteLine($"Lado X: {sideX}");
-                Console.WriteLine($"Lado Y: {sideY}");
-                Console.WriteLine($"Lado Z: {sideZ}");
-                Console.WriteLine("\n/-----------------------------------------\\");
-
-                bool isAValidTriangle = sideX + sideY > sideZ && sideX + sideZ > sideY && sideY + sideZ > sideX;
-
-                if (isAValidTriangle == true)
+                if (IsAnTriangle(sideX, sideY, sideZ))
                 {
-                    Console.WriteLine("\nÉ um triângulo válido.");
-                    string triangleType = "Não classificado";
+                    string triangleType = FindTriangleType(sideX, sideY, sideZ);
 
-                    if (sideX == sideX && sideY == sideZ)
-                    {
-                        triangleType = "Equilátero";
-                    }
-                    else if (sideX != sideY && sideY != sideZ && sideX != sideZ)
-                    {
-                        triangleType = "Escaleno";
-                    }
-                    else
-                        triangleType = "Isósceles";
 
                     Console.WriteLine($"O triângulo é {triangleType}.");
                 }
                 else
                     Console.WriteLine("\nOs valores informados não formam um triâgulo válido");
 
-                Console.Write("\nDeseja continuar? (S/N) ");
-                string optionContinue = Console.ReadLine()!.ToUpper();
+                string optionContinue = ShowContinue();
                 if (optionContinue != "S")
                 {
                     Console.Clear();
@@ -64,6 +40,55 @@
             Console.WriteLine("/-----------------------------------------\\");
             Console.WriteLine("        Classificador de Triângulos");
             Console.WriteLine("/-----------------------------------------\\\n");
+        }
+        static int InputReader(string side)
+        {
+            int value;
+            do
+            {
+                Console.Write($"Informe o valor do {side}: ");
+                bool successfulyConverted = int.TryParse(Console.ReadLine()!, out value);
+                if (successfulyConverted && value > 0)
+                    break;
+
+                Console.WriteLine("\n/---------------------------------------------------------------------\\");
+                Console.WriteLine("Entrada inválida! Por favor, informe apenas números inteiros positivos.");
+                Console.WriteLine("/---------------------------------------------------------------------\\\n");
+            } while (true);
+
+            return value;
+        }
+        static void ShowValues(int sideX, int sideY, int sideZ)
+        {
+            Console.WriteLine("Valores Informados:");
+            Console.WriteLine($"Lado X: {sideX}");
+            Console.WriteLine($"Lado Y: {sideY}");
+            Console.WriteLine($"Lado Z: {sideZ}");
+            Console.WriteLine("\n/-----------------------------------------\\");
+        }
+        static bool IsAnTriangle(int sideX, int sideY, int sideZ)
+        {
+            return sideX + sideY > sideZ && sideX + sideZ > sideY && sideY + sideZ > sideX;
+        }
+        static string FindTriangleType(int sideX, int sideY, int sideZ)
+        {
+            if (sideX == sideY && sideY == sideZ)
+            {
+                return "Equilátero";
+            }
+            else if (sideX != sideY && sideY != sideZ && sideX != sideZ)
+            {
+                return "Escaleno";
+            }
+            else
+                return "Isósceles";
+        }
+        static string ShowContinue()
+        {
+            Console.Write("\nDeseja continuar? (S/N) ");
+            string optionContinue = Console.ReadLine()!.ToUpper();
+            return optionContinue;
+
         }
     }
 }
